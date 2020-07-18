1cbfdd5407254415ac31a481ab8d9699d738cc03
ManideepNadimpelli <36722168+ManideepNadimpelli@users.noreply.github.com>
2020-07-18 09:22:38 -0500

    MICP4

---
+++ Mobile_ICP4/AndroidHardware/app/src/main/java/com/example/vijaya/androidhardware/StorageActivity.java
@@ -0,0 +1,84 @@
+package com.example.vijaya.androidhardware;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StorageActivity extends AppCompatActivity {
+    EditText txt_content;
+    EditText contenttoDisplay;
+    String FILENAME = "MyAppStorage";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage);
+        txt_content = (EditText) findViewById(R.id.id_txt_mycontent);
+        contenttoDisplay = (EditText) findViewById(R.id.id_txt_display);
+    }
+
+    public void saveTofile(View v) throws IOException {
+
+        String text = txt_content.getText().toString();
+        FileOutputStream fos = null;
+        // ICP Task4: Write the code to save the text
+        try {
+            fos = openFileOutput(FILENAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            txt_content.getText().clear();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILENAME,
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void retrieveFromFile(View v) throws IOException {
+        FileInputStream fis = null;
+        // ICP Task4: Write the code to display the above saved text
+        try {
+            fis = openFileInput(FILENAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+            txt_content.setText(sb.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
