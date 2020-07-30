9578634cfe0f9765b9a3a18876df190a17a2ddae
ManideepNadimpelli <36722168+ManideepNadimpelli@users.noreply.github.com>
2020-07-30 01:37:33 -0500

    changes

--- MobileProject/SpinTheBottle/app/src/main/java/com/example/spinthebottle/RegisterActivity.java
+++ MobileProject/SpinTheBottle/app/src/main/java/com/example/spinthebottle/RegisterActivity.java
@@ -75,7 +75,7 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(RegisterActivity.this, "User Created", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            startActivity(new Intent(getApplicationContext(), LogInActivity.class));
                         } else {
                             Toast.makeText(RegisterActivity.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.GONE);
