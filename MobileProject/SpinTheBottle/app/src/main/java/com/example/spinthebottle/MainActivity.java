1b3b0fa455e6e04e0dcce45d2e34a9d5337be604
ManideepNadimpelli <36722168+ManideepNadimpelli@users.noreply.github.com>
2020-07-30 02:37:28 -0500

    final

--- MobileProject/SpinTheBottle/app/src/main/java/com/example/spinthebottle/MainActivity.java
+++ MobileProject/SpinTheBottle/app/src/main/java/com/example/spinthebottle/MainActivity.java
@@ -4,12 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Random;
 
@@ -54,9 +67,99 @@ public class MainActivity extends AppCompatActivity {
 
             lastDir = newDir;
             bottle.startAnimation(rotate);
-        }
-    }
 
+            float remain = (float) newDir / (float) 360 % 1;
+            System.out.println(remain);
+
+            if (remain > 0.25 & remain < 0.75) {
+                System.out.println("Pokemon");
+                String poke_win = ("Pokemon won");
+                TextView winner = findViewById(R.id.textView4);
+                winner.setText(poke_win);
+
+            } else {
+                System.out.println("Yu Gi Oh");
+                String yugi_win = ("Yu Gi Oh won");
+                TextView winner = findViewById(R.id.textView4);
+                winner.setText(yugi_win);
+            }
+        }
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        String URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response", response.toString());
+                        System.out.println(response);
+                        try {
+                            Random num = new Random();
+                            JSONArray results = response.getJSONArray("results");
+                            JSONObject index = results.getJSONObject(num.nextInt(150));
+                            String name = index.getString("name");
+                            System.out.println(name);
+                            TextView output = findViewById(R.id.textView3);
+                            output.setText(name);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+
+        RequestQueue requestQueue2 = Volley.newRequestQueue(this);
+
+        String URL2 = "https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Normal Monster";
+
+        JsonObjectRequest objectRequest2 = new JsonObjectRequest(
+                Request.Method.GET,
+                URL2,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response", response.toString());
+                        System.out.println(response);
+                        try {
+                            Random num = new Random();
+                            JSONArray results = response.getJSONArray("data");
+                            JSONObject index = results.getJSONObject(num.nextInt(50));
+                            String name = index.getString("name");
+                            System.out.println(name);
+                            TextView output2 = findViewById(R.id.textView2);
+                            output2.setText(name);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+
+                    }
+                }
+        );
+
+        requestQueue2.add(objectRequest2);
+    }
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(), LogInActivity.class));
